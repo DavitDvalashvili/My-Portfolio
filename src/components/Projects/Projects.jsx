7bc6476667f8bb16974a1af84eb7201b73445b0c
davitdvalashvili <davitdvalashvili1996@gmail.com>
2023-10-27 03:51:41 +0400

    feat: finish projects page

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,88 +1,98 @@
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import Audiophile from "../../assets/Projects/Audiophile-eCommerce-website.png";
-import Photosnap from "../../Assets/Projects/Photosnap-multi-page-website.png";
-import NightCup from "../../Assets/Projects/Knight-Cup-Registration.png";
-import Dictionary from "../../Assets/Projects/Dictionary-web-app.png";
-import Planets from "../../Assets/Projects/Planets-fact-site.png";
-import Todo from "../../Assets/Projects/Todo-App.png";
+import styled from "styled-components";
+import data from "./../../DataCard.json";
 
 const Projects = () => {
   return (
-    <Container fluid className="project-section">
-      <Particle />
-      <Container>
-        <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
-        </h1>
-        <p style={{ color: "white" }}>
-          Here are a few projects I have worked on recently.
-        </p>
-        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Audiophile}
-              title="Audiophile Ecommerce website"
-              description="Built with: Typescript, Styled-Components, React-Router, React-hook-form, React-Helmet"
-              ghLink="https://github.com/DavitDvalashvili/Audiophile-ecommerce-website"
-              demoLink="https://audiophile-ecommerce-website-lake.vercel.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Photosnap}
-              title="Photosnap multi page website"
-              description="Built wth: React Javascript Library, Typescript, Styled-Components, React-Router"
-              ghLink="https://github.com/DavitDvalashvili/Photosnap-multi-page-website"
-              demoLink="https://photosnap-multi-page-website-two.vercel.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={NightCup}
-              title="RedBerry-Knight-Cup-Registration"
-              description="Built with: Typescript, Styled-Components, React Router, React-hook-form"
-              ghLink="https://github.com/DavitDvalashvili/RedBerry-Knight-Cup-Registration"
-              demoLink="https://red-berry-knight-cup-registration-nine.vercel.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Dictionary}
-              title="Dictionary-web-app"
-              description="Built with: Javascript, React Javascript Library, Tailwind for styling, Rest-Api"
-              ghLink="https://github.com/soumyajit4419/Plant_AI"
-              demoLink="https://plant49-ai.herokuapp.com/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Planets}
-              title="Planets fact site"
-              description="Built with: Typescript, React Javascript Library, Styled-Components"
-              ghLink="https://github.com/DavitDvalashvili/Planets-fact-site"
-              demoLink="https://planets-fact-site-theta.vercel.app/"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Todo}
-              title="Todo-app.png"
-              description="Built with: Vanila Javascript, SASS - css pre-processor, html"
-              ghLink="https://davitdvalashvili.github.io/Todo-App/"
-              demoLink="https://github.com/DavitDvalashvili/Todo-App"
-            />
-          </Col>
-        </Row>
+    <ProjectStyle>
+      <Container fluid className="project-section">
+        <Particle />
+        <Container>
+          <h1 className="project-heading">
+            My Recent <strong className="purple">Works </strong>
+          </h1>
+          <p style={{ color: "white" }}>
+            Here are a few projects I have worked on recently.
+          </p>
+          <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+            {data.map((card) => (
+              <Col md={4} className="project-card" key={card.id}>
+                <ProjectCard
+                  imgPath={card.image}
+                  title={card.title}
+                  stack={card.stacks}
+                  description={card.description}
+                  ghLink={card.githubLink}
+                  demoLink={card.demoLink}
+                />
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </Container>
-    </Container>
+    </ProjectStyle>
   );
 };
 
 export default Projects;
+
+const ProjectStyle = styled.section`
+  .project-section {
+    position: relative !important;
+    padding-top: 150px !important;
+    padding-bottom: 30px !important;
+    background-image: var(--section-background-color) !important;
+  }
+
+  .project-card {
+    padding-top: 60px !important;
+    padding-bottom: 60px !important;
+    padding-left: 30px !important;
+    padding-right: 30px !important;
+    height: auto !important;
+  }
+
+  .project-card-view {
+    box-shadow: 0 4px 5px 3px rgba(119, 53, 136, 0.459) !important;
+    color: white !important;
+    background-color: transparent !important;
+    opacity: 0.9 !important;
+    transition: all 0.5s ease 0s !important;
+    height: 100% !important;
+  }
+  .project-card-view:hover {
+    transform: scale(1.02) !important;
+    overflow: hidden !important;
+    box-shadow: 0 5px 5px 6px rgba(129, 72, 144, 0.561) !important;
+  }
+
+  .card-img-top {
+    padding: 20px !important;
+    opacity: 0.8 !important;
+    border-radius: 10px !important;
+  }
+
+  .btn-primary {
+    color: #fff !important;
+    background-color: #623686 !important;
+    border-color: #623686 !important;
+  }
+
+  .btn-primary:hover {
+    color: #fff !important;
+    background-color: #6d20c5d7 !important;
+    border-color: #6d20c5d7 !important;
+  }
+  .btn:focus {
+    outline: none;
+    box-shadow: none;
+  }
+  .project-heading {
+    color: white;
+    font-size: 2.5em;
+    font-weight: 500;
+    padding-top: 10px;
+  }
+`;
